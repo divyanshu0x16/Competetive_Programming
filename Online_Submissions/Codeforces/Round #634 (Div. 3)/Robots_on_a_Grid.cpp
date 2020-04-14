1724731a18da7c61bc3d529ef8f8aa2551cf9b1d
divyanshu0x16 <divyanshu22.dm@gmail.com>
2020-04-14 18:33:59 +0530

    All submissions in one folder

---
+++ Online_Submissions/Codeforces/Round #634 (Div. 3)/Robots_on_a_Grid.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp> 
+using namespace std;
+using namespace __gnu_pbds;
+/* Solution by divyanshu0x16*/
+#define int           long long 
+#define setbits(x)    __builtin_popcountll(x)
+#define zerobits(x)   __builtin_ctzll(x)
+#define mod           1000000007 
+#define ps(x, y)      fixed<<setprecision(y)<<x
+#define testcase(x)   int x; cin >> x; while(x--)
+#define fastIO        ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define ordered_set   tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+typedef pair<int, int> pii;
+typedef pair<string, string> pss;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef stack<int> si;
+typedef queue<int> qi;
+typedef priority_queue<int> pqi;
+
+
+int32_t main() {
+    fastIO;
+    testcase(t){
+        #include <bits/stdc++.h>
+        #include <ext/pb_ds/assoc_container.hpp>
+        #include <ext/pb_ds/tree_policy.hpp> 
+        using namespace std;
+        using namespace __gnu_pbds;
+        /* Solution by divyanshu0x16*/
+        #define int           long long 
+        #define setbits(x)    __builtin_popcountll(x)
+        #define zerobits(x)   __builtin_ctzll(x)
+        #define mod           1000000007 
+        #define ps(x, y)      fixed<<setprecision(y)<<x
+        #define testcase(x)   int x; cin >> x; while(x--)
+        #define fastIO        ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+        #define ordered_set   tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+        typedef pair<int, int> pii;
+        typedef pair<string, string> pss;
+        typedef vector<int> vi;
+        typedef vector<vi> vvi;
+        typedef vector<pii> vii;
+        typedef stack<int> si;
+        typedef queue<int> qi;
+        typedef priority_queue<int> pqi;
+        
+        
+        int32_t main() {
+            fastIO;
+            testcase(t){
+                
+            }
+            return 0;
+        }
+         
+    }
+    return 0;
+}
+ 
