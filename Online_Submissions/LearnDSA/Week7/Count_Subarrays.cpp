022734fbc74f20cee25d413dd9a9931325e6b21a
divyanshu0x16 <divyanshu22.dm@gmail.com>
2020-05-12 02:33:07 +0530

    Private Backup

---
+++ Online_Submissions/LearnDSA/Week7/Count_Subarrays.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp> 
+using namespace std;
+using namespace __gnu_pbds;
+/* Solution by divyanshu0x16*/
+#define int           long long 
+#define setbits(x)    __builtin_popcountll(x)
+#define zerobits(x)   __builtin_ctzll(x)
+#define mod           1000000007 
+#define modulo(a, b)  (a%b<0 ? a%b+b : a%b)
+#define intpow(a,b)   (int)(pow(a,b) + 0.5)
+#define gcd(a,b)      __gcd(a, b)
+#define all(x)        (x).begin(), (x).end()
+#define ps(x, y)      fixed<<setprecision(y)<<x
+#define testcase(x)   int x; cin >> x; while(x--)
+#define fastIO        ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define ordered_set   tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+typedef pair<int, int> pii;
+typedef pair<string, string> pss;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef stack<int> si;
+typedef queue<int> qi;
+typedef priority_queue<int> pqi;
+
+
+int32_t main() {
+    fastIO;
+    testcase(t){
+        int n; cin >> n;
+        vi array;
+        array.push_back(0);
+        for (int i = 1; i <= n; i++)
+        {
+            int input; cin >> input;
+            array.push_back(input);
+        }
+        vector<int> no_of_non_decreasing_subarray_till(n+1,0);
+        for (int i = 1; i <= n; i++)
+        {
+            no_of_non_decreasing_subarray_till[i] = (array[i] >= array[i-1] ? no_of_non_decreasing_subarray_till[i-1] +1 : 1);
+        }
+        int ans = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            ans += no_of_non_decreasing_subarray_till[i];
+        }
+        cout << ans << "\n";
+    }
+    return 0;
+}
+ 
