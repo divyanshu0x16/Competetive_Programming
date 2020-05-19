568b91717d713a45157863a1cdaf55d735043ba2
divyanshu0x16 <divyanshu22.dm@gmail.com>
2020-05-19 17:42:50 +0530

    Library

---
+++ Library/DSA/Topological_Sort.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+int n;
+vector<vector<int>> adj;
+vector<bool> visited;
+vector<int> ans;
+
+void dfs(int v){
+    visited[v] = true;
+    for(int u : adj[v]){
+        if(!visited[u])
+            dfs(u);
+    }
+    ans.push_back(v);
+}
+
+void topological_sort(){
+    visited.assign(n, false);
+    ans.clear();
+    for (int i = 0; i < n; i++)
+    {
+        if(!visited[i])
+            dfs(i);
+    }
+    reverse(ans.begin(),ans.end());
+}
+
+
+int main() {
+   
+    return 0;
+}
+ 
